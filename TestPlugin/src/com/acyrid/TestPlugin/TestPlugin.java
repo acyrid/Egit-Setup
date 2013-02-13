e8de2ce6be433ae033b997e1d3a3e82b5369815e
acyrid <acyrid@gmail.com>
2013-02-13 03:35:53 -0600

    Setting up Git

---
+++ TestPlugin/src/com/acyrid/TestPlugin/TestPlugin.java
@@ -0,0 +1,34 @@
+package com.acyrid.TestPlugin;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        // TODO: Place any custom disable code here
+
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        getLogger().info("Goodbye world!");
+    }
+
+    @Override
+    public void onEnable() {
+        // TODO: Place any custom enable code here including the registration of any events
+
+        // Register our events
+        PluginManager pm = getServer().getPluginManager();
+
+        // Register our commands
+        // Brah
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        getLogger().info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+    }
+
+}
